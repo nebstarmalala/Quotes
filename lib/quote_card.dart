c41407d5ee08022ac3e95ceee6f9664acbefdff2
Nebstar Malala <56993122+nebstarmalala@users.noreply.github.com>
2021-10-07 14:13:57 +0300

    add delete functionality to button

--- lib/quote_card.dart
+++ lib/quote_card.dart
@@ -5,7 +5,8 @@ import 'quote.dart';
 class QuoteCard extends StatelessWidget {
 
   final Quote quote;
-  QuoteCard({ this.quote });
+  final Function delete;
+  QuoteCard({ this.quote, this.delete });
 
   @override
   Widget build(BuildContext context) {
@@ -23,13 +24,19 @@ class QuoteCard extends StatelessWidget {
                 color: Colors.grey[600],
               ),
             ),
-            SizedBox(height: 6.0,),
+            SizedBox(height: 6.0),
             Text(
               quote.author,
               style: TextStyle(
                   fontSize: 14.0,
                   color: Colors.grey[800]
               ),
+            ),
+            SizedBox(height: 8.0),
+            FlatButton.icon(
+                onPressed: delete,
+                icon: Icon(Icons.delete),
+                label: Text("Delete Quote")
             )
           ],
         ),
